7788849696d5612e9276643fc078b3e9da99def9
kaz3w.ubu20 <katsumi@ubu20.local.kaz3w.net>
2022-08-11 10:40:50 +0900

    update

--- helper_script_for_armhf.sh
+++ helper_script_for_armhf.sh
@@ -16,8 +16,8 @@ if [ "$architecture" = "armhf" ]; then
                                   software-properties-common \
                                   wget
   wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null && \
-  apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main' && \
-  apt-get update && apt-get install -y cmake=3.19.2-0kitware1ubuntu20.04.1 \
-                                       cmake-data=3.19.2-0kitware1ubuntu20.04.1
+  apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main' && \
+  apt-get update && apt-get install -y cmake=3.19.2-0kitware1ubuntu18.04.1 \
+                                       cmake-data=3.19.2-0kitware1ubuntu18.04.1
 fi
 
